23ae912173023e5c87cdfdaa795a5fa1d774c780
Ornie <orniepay@buffalo.edu>
2024-03-20 17:53:20 -0400

    site connectivity checker project

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import ssl
 import urllib.request as urllib
 
 def main(url):
+    """
+    Checks the connectivity to a specified URL.
+
+    This function attempts to establish a connection to the given URL to check 
+    its accessibility over the internet. It bypasses SSL certificate verification 
+    for HTTPS connections, which is useful for testing purposes but not 
+    recommended for secure or production environments. The function reports 
+    back the HTTP response code if the connection is successful, or the error 
+    if it fails to connect.
+
+    Parameters:
+    url (str): The URL to be checked for connectivity.
+
+    Returns:
+    None: The function prints the result of the connectivity check to the console
+    and does not return a value.
+    """
+    
     print("Checking connectivity...")
 
     context = ssl._create_unverified_context() # SSL context that bypasses certificate verification
